2cdcf638401f301048af0952dc406e88ba9fb31a
ealgase <mostdigitsofpi@gmail.com>
2015-03-25 07:13:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@
 		ext.load_hqs = function(){ScratchExtensions.loadExternalJS("https://cdn.rawgit.com/MegaApuTurkUltra/Scratch-HQ-Sound/c9bc589fd176ba6f466b1d3141eb67370dfc0a0f/scratch-hqs.ext.js")};
         ext.load_feedme = function(){(function(){var s=document.createElement('script');s.src='https://docs.google.com/uc?authuser=0&id=0B_GQBg4bWXTnY09wNFAxTV9NUTg&export=download';document.body.appendChild(s);})()};
 	    ext.load_kva = function(){(function (){var s = document.createElement('script');s.setAttribute('src', 'https://eddb88dd722854829255ed75db01256d69cfa6bb-www.googledrive.com/host/0B4nqFvjFmccQdHVTeXpOOTdqMHM');document.body.appendChild(s);})();};
-	    ext.load_alm = function(){ScratchExtensions.loadExternalJS("https://cdn.rawgit.com/LLK/scratch-extension-docs/master/alarm_extension.js");};
+	    ext.load_brs = function(){ScratchExtensions.loadExternalJS("https://cdn.rawgit.com/LLK/scratch-extension-docs/master/browser_extension.js");};
+	    ext.load_cht = function(){ScratchExtensions.loadExternalJS("http://textuploader.com/07bg/raw");};
 	
 	    var descriptor = {
         blocks: [
@@ -22,6 +23,8 @@
 			[' ','load extension FeedMe', 'load_feedme'],
 			[' ','load extension Kvaxtension', 'load_kva'],
 			[' ','load extension Alarm Extension', 'load_alm'],
+			[' ','load extension Browser Extension', 'load_brs'],
+			[' ','load extension Coool Helpful Things Extension', 'load_cht'],
 		    ['b','load extension extension installed?','load_ext']	
 		],
         menus: {
